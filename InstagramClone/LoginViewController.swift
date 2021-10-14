c3cca21c774020914508508fb4f20f36e553a258
Jacob Feigenheimer <jlfeigenheimer@gmail.com>
2021-10-13 20:51:22 -0400

    Created signing/signup for segue

--- InstagramClone/LoginViewController.swift
+++ InstagramClone/LoginViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
-
+    
+    
     @IBOutlet weak var userNameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +20,37 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func onSignIn(_ sender: Any) {
+    @IBAction func signIn(_ sender: Any) {
+        let username = userNameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil
+            {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else
+            {
+                print("Print \(error?.localizedDescription)")
+            }
+        }
     }
-    
-    @IBAction func onSignUp(_ sender: Any) {
+    @IBAction func signUp(_ sender: Any)
+    {
+        let user = PFUser()
+        user.username = userNameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success,error) in
+            if success
+            {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else
+            {
+                print("Print \(error?.localizedDescription)")
+            }
+        }
     }
     /*
     // MARK: - Navigation
